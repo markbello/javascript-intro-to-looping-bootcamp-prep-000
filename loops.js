ec14b78d6c51735f0f3aab707627ee5cd339f126
Mark Bello <mcbello51286@gmail.com>
2017-11-16 22:10:30 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array){
+  for(let i = 0; i =< 25; i++){
+    if(i == 1){
+      arr.push('I am 1 strange loop.');
+    } else{
+      arr.push(`I am ${i} strange loops.`);
+    }
+  }
+}
+
+function whileLoop(n){
+  while(n > 0){
+    console.log(n);
+    n--;
+  }
+}
+
+function doWhileLoop(arr){
+  function maybeTrue() {
+    return Math.random() >= 0.5;
+  }
+  do{
+    arr.shift();
+  } while(arr.length > 0 && maybeTrue());
+  return arr;
+}
